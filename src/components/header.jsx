c5eae45bdcae8091d760a4af0fd518d369324ba2
Diego Tavares <mello.gredy@gmail.com>
2022-09-08 00:10:27 -0300

    Finalizando...

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -34,16 +34,7 @@ function Header() {
                 dignissim. Suspendisse semper pretium consectetur. Praesent
                 bibendum arcu risus, sit amet iaculis ex tempus quis. Cras et
                 erat ut tellus vulputate tincidunt. Aenean lacinia euismod augue
-                vel egestas. Class aptent taciti sociosqu ad litora torquent per
-                conubia nostra, per inceptos himenaeos. Vestibulum vel urna
-                tortor. Vivamus et arcu non felis tristique eleifend. Morbi eu
-                condimentum urna. Curabitur eu magna eget turpis condimentum
-                ultrices. Suspendisse quis lorem ultricies, pulvinar purus sed,
-                egestas erat. Etiam ultricies a ante vehicula pharetra. Quisque
-                ut neque mattis, consequat velit ut, ultrices orci. Nulla varius
-                elementum erat vel pharetra. Aenean sit amet nisi diam. Morbi
-                viverra, magna ac luctus commodo, odio ante suscipit libero, at
-                mattis augue est vel metus.{" "}
+                vel egestas.{" "}
               </p>
             </div>
             <div className="items-form">
